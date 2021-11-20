af9fb815909522eb34029042e2e3239b36d084b4
Kateryna Karpova <katyakarpova@gmail.com>
2021-11-20 14:05:24 +0100

    Step 5

--- contacts.js
+++ contacts.js
@@ -1,46 +1,49 @@
 const fs = require("fs/promises");
 const path = require("path");
 
-const contactsPath = path.join(__dirname, "./db/contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const newContacts = async (contacts) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
 };
 
-// TODO: задокументировать каждую функцию
-
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
   const contacts = JSON.parse(data);
-  //console.log("Все контакты:", contacts);
   return contacts;
 }
 
 async function getContactById(contactId) {
   const contacts = await listContacts();
-  const contactById = contacts.find((contact) => contact.id === contactId);
+  const contactById = contacts.find(
+    (contact) => String(contact.id) === contactId
+  );
   if (!contactById) {
     return null;
   }
-  //console.log("Контакт с айди 3:", contactById);
   return contactById;
 }
 
 async function removeContact(contactId) {
   const contacts = await listContacts();
-  const updatedContacts = contacts.filter(
-    (contact) => contact.id !== contactId
-  );
+  const idx = contacts.findIndex((contact) => String(contact.id) === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const updatedContacts = contacts.filter((_, index) => index !== idx);
   await newContacts(updatedContacts);
-  //console.log("Новый список контактов:", updatedContacts);
+  return contacts[idx];
 }
 
 async function addContact(name, email, phone) {
   const contacts = await listContacts();
-  const newContact = { name, email, phone };
+  const newContact = {
+    name,
+    email,
+    phone,
+  };
   contacts.push(newContact);
   await newContacts(contacts);
-  console.log("Новый контакт:", newContact);
   return newContact;
 }
 
@@ -48,5 +51,5 @@ module.exports = {
   listContacts,
   getContactById,
   removeContact,
-  addContact
+  addContact,
 };
